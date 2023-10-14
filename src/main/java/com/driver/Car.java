789a305ecae10bbd0b8531c7ae3fee4d8b3f21b4
SIMANSHU CHAUBEY <76093178+simanshu@users.noreply.github.com>
2023-10-14 22:26:13 +0530

    Update Car.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -9,7 +9,7 @@ public class Car extends Vehicle {
     private int currentGear;
     private int seats;
 
-    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+   public Car(String name,boolean isManual) {
         //Hint: Car extends Vehicle
         super(name);
         this.currentGear=1;
